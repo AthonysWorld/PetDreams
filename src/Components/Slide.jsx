cde365276105bd909de646644f1df1b1368fe8b1
Adams Anthony Isioma <114524110+AthonysWorld@users.noreply.github.com>
2023-08-28 12:25:55 +0100

    Add files via upload

--- src/Components/Slide.jsx
+++ src/Components/Slide.jsx
@@ -1,7 +1,5 @@
-
 import Pic from '../images/landing dog.png'
-import Pic1 from '../images/parrot.png'
-import Pic2 from '../images/pitbull.png'
+
 const Slide = () => {
   return (
     <div className='top'>
@@ -17,27 +15,10 @@ I hope you find all the information you need to make the best<br/>decision for y
 
 </div>
 <div className='container one col-lg-6 col-md-6'>
-<div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-inner">
-    <div className="carousel-item active" data-bs-interval="1500">
-      <img src={Pic} className="d-block w-100" alt="..1."/>
-    </div>
-    <div className="carousel-item" data-bs-interval="1500">
-      <img src={Pic1} className="d-block w-100" alt=".2.."/>
-    </div>
-    <div className="carousel-item" data-bs-interval="1500">
-      <img src={Pic2} className="d-block w-100" alt=".3.."/>
-    </div>
-  </div>
-  <button className="carousel-control-prev slidee-btn" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next slidee-btn" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
+<img className='Picturetwo' src={Pic}/>
+
+
+
 </div>
 
     </div> 
