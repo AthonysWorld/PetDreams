c95b63029c31b8139dd7fe7d4ace59fee7c555b4
Adams Anthony Isioma <114524110+AthonysWorld@users.noreply.github.com>
2023-08-29 10:59:00 +0100

    Add files via upload

--- src/Components/Store.jsx
+++ src/Components/Store.jsx
@@ -1,54 +1,85 @@
-import Pic from '../images/cardone.png'
-import Pic1 from '../images/cardtwo.png'
-import Pic2 from '../images/whitegsd.jpg'
-import Pic3 from '../images/lab.jpg'
-import Pic4 from '../images/PUG.jpg'
+import Pic from '../images/product1.jpg'
+import Pic1 from '../images/product2.jpg'
+import Pic2 from '../images/product3.jpg'
+import Pic3 from '../images/product4.jpg'
+import Pic4 from '../images/product6.jpg'
+import Pic5 from '../images/product7.jpg'
+import Pic6 from '../images/product8.jpg'
+
 
 const Store = () => {
   return (
     <div className='third'>
-    <h1>Available pets</h1>
+    <h1>Pet Essentials</h1>
       <div className="container-fluid third row">
       <div className="container col-lg-3 col-5 ml-auto">
 <div className='card cardy'>
-<img src={Pic3} className="card-img-top"/>
-   <div className="card-body">
-   <h3>Lab</h3>
-<button className='btn'>See pet</button>
+<img src={Pic3} className="card-img-top storex"/>
+<p>hello</p>
+<button className='btn'>View</button>
 
-   </div>
 </div>
       </div>
       <div className="container col-lg-3 col-5">
       <div className='card cardy'>
     <img src={Pic4} className="card-img-top"/>
-    <div className="card-body">
-   <h3>Pug</h3>
-   <button className='btn'>See pet</button>
+    <p>hello</p>
+   <button className='btn'>View</button>
 
-   </div>
 </div>
 </div>
 <div className="container col-lg-3 col-5">
 <div className='card cardy'>
     <img src={Pic2} className="card-img-top"/>
-    <div className="card-body">
-   <h3>German shepherd</h3>
-   <button className='btn'>See pet</button>
-
-   </div>
+    <p>hello</p>
+   <button className='btn'>View</button>
 </div>
 </div>
 <div className="container col-lg-3 col-5">
 <div className='card cardy'>
     <img src={Pic3} className="card-img-top"/>
-    <div className="card-body">
-   <h3>Lab</h3>
-   <button className='btn'>See pet</button>
+    <p>hello</p>
+   <button className='btn'>View</button>
+</div>
+</div>
 
-   </div>
+<div className="container col-lg-3 col-5">
+      <div className='card cardy'>
+    <img src={Pic4} className="card-img-top"/>
+    <p>hello</p>
+   <button className='btn'>View</button>
+
 </div>
 </div>
+<div className="container col-lg-3 col-5">
+<div className='card cardy'>
+    <img src={Pic5} className="card-img-top"/>
+   
+    <p>hello</p>
+   <button className='btn'>View</button>
+
+</div>
+</div>
+<div className="container col-lg-3 col-5">
+<div className='card cardy'>
+    <img src={Pic6} className="card-img-top"/>
+    
+    <p>hello</p>
+   <button className='btn'>View</button>
+
+</div>
+</div>
+<div className="container col-lg-3 col-5">
+<div className='card cardy'>
+    <img src={Pic1} className="card-img-top"/>
+    
+ <p>hello</p>
+   <button className='btn'>view</button>
+
+   
+</div>
+</div>
+
       </div>{/* fluid ends */}
     </div>
   )
